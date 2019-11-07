6d34854ca5767a98cd3b0dbaaf9185a3f3c96661
Student <example@example.com>
2019-11-07 14:23:59 -0800

    Set up basic HTML layout for game. Grid only.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+    console.log( "ready!" );
+});
+
+
+
+
+// General Requirements:
+//
+// 1. Switch player back and forth  Manage the active player between 1 and 0.
+// 2. Board Layout - this is going to be a constructor - Three rows: Top, Middle, Bottom (all arrays)
+// 3. Indicate if X or O mark depending on who is playing
+// 4. Click a card, register what card was clicked - corresponding to a spot on the respective array in the object
+// 5. Build board in HTML using cards, Each space to have an ID with a value. Any Direction adding to three or something like that
